299f895dd85addebc1f8d301a77787520bd95bfd
Pavel_M <palich_morozov@mail.ru>
2021-06-06 20:25:51 +1000

    Сделана генерация массива

--- js/main.js
+++ js/main.js
@@ -4,12 +4,45 @@ function getRandomNumber (firstArg, secondArg) {
   return Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
 }
 
-getRandomNumber(30, 60);
-
 function getRandomСoordinate (firstArg, secondArg, numbersAfterPoint = 1) {
   const minRange = Math.min(Math.abs(firstArg), Math.abs(secondArg));
   const maxRange =Math.max(Math.abs(firstArg), Math.abs(secondArg));
   return (maxRange - minRange) ? (Math.random() * (maxRange - minRange) + minRange).toFixed(numbersAfterPoint) : (Math.random() + minRange).toFixed(numbersAfterPoint);
 }
 
-getRandomСoordinate(30, 60, 4);
+
+const TITLE = ['от частного лица', 'от агенства'];
+const TYPE_LODGING = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TIME = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+const getRandomArray = (elements) => elements.slice(getRandomNumber(0,elements.length - 1));
+
+const generateObject = (index) => ({
+  author: {avatar: `img/avatars/user0${  index  }.png`},
+  offer: {
+    title: getRandomArrayElement(TITLE),
+    adress: `${this.location.lat  } ${  this.location.lng}`, // не могу понять как реализовать
+    price: getRandomNumber(5000, 25000),
+    type: getRandomArrayElement(TYPE_LODGING),
+    rooms: getRandomNumber(1, 5),
+    guests: getRandomNumber(1, 9),
+    checkin: getRandomArrayElement(TIME),
+    checkout: getRandomArrayElement(TIME),
+    features: getRandomArray(FEATURES),
+    description: 'Сдается по прекрасной цене',
+    photos: getRandomArray(PHOTOS),
+  },
+  location: {
+    lat: getRandomСoordinate(35.65, 35.7, 5),
+    lng: getRandomСoordinate(139.7, 139.8, 5),
+  },
+});
+
+const createArrayLodging = new Array(10).fill(null).map((value, index) => generateObject(index + 1));
+
+createArrayLodging;
