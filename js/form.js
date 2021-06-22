bac549db340e4abcf59bdaf5fa4382e41a45addf
Pavel_M <palich_morozov@mail.ru>
2021-06-22 14:22:47 +1000

    Сделана валидация формы

--- js/form.js
+++ js/form.js
@@ -1,5 +1,15 @@
+import {typeInform} from './generate-similar-ads.js';
+
 const adForm = document.querySelector('.ad-form');
 const adFormElement = adForm.querySelectorAll('.ad-form__element');
+const adTitleInput = adForm.querySelector('#title');
+const adTypeSelect = adForm.querySelector('#type');
+const adPriceInput = adForm.querySelector('#price');
+const adRoomNumberSelect = adForm.querySelector('#room_number');
+const adCapacitySelect = adForm.querySelector('#capacity');
+const adCapacitySelectOption = adCapacitySelect.querySelectorAll('option');
+const adTimeInSelect = adForm.querySelector('#timein');
+const adTimeOutSelect = adForm.querySelector('#timeout');
 const mapFiltersForm = document.querySelector('.map__filters');
 const mapFiltersFormElements = mapFiltersForm.querySelectorAll('.map__filter');
 const mapFiltersFormFeatures = mapFiltersForm.querySelector('.map__features');
@@ -19,5 +29,86 @@ const activateForm = () => {
   mapFiltersFormElements.forEach((item) => item.removeAttribute('disabled', null));
   mapFiltersFormFeatures.removeAttribute('disabled', null);
 };
+
+const disabledOption = () => {
+  adCapacitySelectOption.forEach((item) => {
+    item.setAttribute('disabled', 'disabled');
+  });
+};
+
+const activateOption = (value) => {
+  adCapacitySelectOption.forEach((item) => {
+    if (value >= item.value && item.value >= 1) {
+      item.removeAttribute('disabled', null);
+    } else if (value >= 100 && item.value <= 0) {
+      item.removeAttribute('disabled', null);
+    }
+  });
+};
+
+const checkRoomsCapacity = () => {
+  if (adRoomNumberSelect.value >= 100) {
+    adCapacitySelect.value = 0;
+  } else if (adCapacitySelect.value > adRoomNumberSelect.value) {
+    adCapacitySelect.value = adRoomNumberSelect.value;
+  } else if (adCapacitySelect.value <= 0) {
+    adCapacitySelect.value = adRoomNumberSelect.value;
+  }
+};
+
+adTitleInput.addEventListener('input', () => {
+  const valueLength = adTitleInput.value.length;
+  const minLengthTitle = adTitleInput.getAttribute('minlength');
+  const maxLengthTitle = adTitleInput.getAttribute('maxlength');
+
+  if (valueLength < minLengthTitle) {
+    adTitleInput.setCustomValidity(`Ещё ${ minLengthTitle - valueLength}`);
+  } else if (valueLength > maxLengthTitle) {
+    adTitleInput.setCustomValidity(`Удалите лишние ${valueLength - maxLengthTitle} симв.`);
+  } else {
+    adTitleInput.setCustomValidity('');
+  }
+
+  adTitleInput.reportValidity();
+});
+
+adTypeSelect.addEventListener('change', (evt) => {
+  adPriceInput.setAttribute('placeholder', typeInform[evt.target.value].minPrice);
+  adPriceInput.setAttribute('min', typeInform[evt.target.value].minPrice);
+});
+
+adPriceInput.addEventListener('input', () =>{
+  const valueInput = adPriceInput.value;
+  const minPrice = adPriceInput.getAttribute('min');
+  const maxPrice = adPriceInput.getAttribute('max');
+
+  if (valueInput < minPrice) {
+    adPriceInput.setCustomValidity(`Минимальная цена ${ minPrice}`);
+  } else if (valueInput > maxPrice) {
+    adPriceInput.setCustomValidity(`Максимальная цена ${ maxPrice}`);
+  } else {
+    adPriceInput.setCustomValidity('');
+  }
+
+  adPriceInput.reportValidity();
+});
+
+adTimeInSelect.addEventListener('change', () => {
+  adTimeOutSelect.value = adTimeInSelect.value;
+});
+
+adTimeOutSelect.addEventListener('change', () => {
+  adTimeInSelect.value = adTimeOutSelect.value;
+});
+
+disabledOption();
+activateOption(adRoomNumberSelect.value);
+checkRoomsCapacity();
+
+adRoomNumberSelect.addEventListener('change', (evt) =>{
+  disabledOption();
+  activateOption(evt.target.value);
+  checkRoomsCapacity();
+});
 
 export {diactivateForm, activateForm};
--- js/generate-similar-ads.js
+++ js/generate-similar-ads.js
@@ -38,12 +38,12 @@ const checkFillData = (firstData, element, selector, otherResult, secondData = 1
   return element.querySelector(selector).classList.add('hidden');
 };
 
-const typeTranslate = {
-  palace: 'Дворец',
-  flat: 'Квартира',
-  house: 'Дом',
-  bungalow: 'Бунгало',
-  hotel: 'Отель',
+const typeInform = {
+  palace: {nameTranslate: 'Дворец', minPrice: 10000},
+  flat: {nameTranslate: 'Квартира', minPrice: 1000},
+  house: {nameTranslate: 'Дом', minPrice: 5000},
+  bungalow: {nameTranslate: 'Бунгало', minPrice: 0},
+  hotel: {nameTranslate: 'Отель', minPrice: 3000},
 };
 
 similarAds.forEach(({author: {avatar}, offer: {title, address, price, type, rooms, guests, checkin, checkout, features, description, photos}}) => {
@@ -51,7 +51,7 @@ similarAds.forEach(({author: {avatar}, offer: {title, address, price, type, room
   checkFillData(title, adElement, '.popup__title');
   checkFillData(address, adElement, '.popup__text--address');
   checkFillData(price, adElement, '.popup__text--price', `${price  } ₽/ночь`);
-  checkFillData(type, adElement, '.popup__type', typeTranslate[type]);
+  checkFillData(type, adElement, '.popup__type', typeInform[type].nameTranslate);
   checkFillData(rooms, adElement, '.popup__text--capacity', `${rooms  } комнаты для ${  guests  } гостей`, guests);
   checkFillData(checkin, adElement, '.popup__text--time', `Заезд после ${ checkin}, выезд до ${ checkout}`, checkout);
   renderFeaturesAd(adElement, features);
@@ -62,3 +62,5 @@ similarAds.forEach(({author: {avatar}, offer: {title, address, price, type, room
 });
 
 mapCanvas.appendChild(similarListFragment);
+
+export {typeInform};
