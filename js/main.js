325c643ddb795af1bf522bd3709de9cb03eaf777
Pavel_M <palich_morozov@mail.ru>
2021-06-25 11:02:46 +1000

    На карту добавлена главная метка

--- js/main.js
+++ js/main.js
@@ -12,3 +12,22 @@ L.tileLayer(
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 ).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '../img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const MainPinmarker = L.marker(
+  {
+    lat: 35.6894,
+    lng: 139.692,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+MainPinmarker.addTo(map);
